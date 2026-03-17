f8f67aec858ddfa344a2d4006682bf21781216f9
iTzFrancesco <francescodcarancio11@gmail.com>
2026-03-17 21:31:33 +0100

    discord invite link fix

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -2,7 +2,7 @@ export const SITE_CONFIG = {
   serverIP: "play.eclipsenetwork.it",
   serverName: "EclipseNetwork",
   discordInvite: "https://discord.gg/cU6x8t49B3",
-  discordInviteCode: "HDxBHTjmcT",
+  discordInviteCode: "cU6x8t49B3",
   storeUrl: "https://store.eclipsenetwork.it",
   modesMaxWidth: "72rem",
 };
